e0dce304f73acfa52bd0392a7542e6d5aa38c53e
Erickype <erickype@hotmail.com>
2022-09-26 14:28:24 -0500

    Use of wait group done

--- routines/methods.go
+++ routines/methods.go
@@ -3,12 +3,15 @@ package routines
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
-func ShowGoRoutine(id int) {
+func ShowGoRoutine(id int, wg *sync.WaitGroup) {
 	delay := rand.Intn(500)
 	fmt.Printf("Goroutine #%v with %vms delay.\n", id, delay)
 
 	time.Sleep(time.Millisecond * time.Duration(delay))
+
+	wg.Done()
 }
