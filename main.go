c5c9aab9ecafa9139ea9304549f6d94461b8bbe5
Erickype <erickype@hotmail.com>
2022-09-26 14:18:39 -0500

    Use of the ShowGoRoutine with goroutine and a manual delay

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/Erickype/GoRoutines/routines"
+import (
+	"github.com/Erickype/GoRoutines/routines"
+	"time"
+)
 
 func main() {
 	for i := 0; i < 10; i++ {
-		routines.ShowGoRoutine(i)
+		go routines.ShowGoRoutine(i)
 	}
+
+	time.Sleep(time.Minute)
 }
