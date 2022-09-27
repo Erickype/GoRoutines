07505828293e4f552668654a8e72838990b1369c
Erickype <erickype@hotmail.com>
2022-09-27 15:50:21 -0500

    FindBook function implemented

--- data/data.go
+++ data/data.go
@@ -5,3 +5,18 @@ type Book struct {
 	Title    string
 	Finished bool
 }
+
+var books = []*Book{}
+
+func FindBook(id int) (int, *Book) {
+	index := -1
+	var book *Book
+
+	for i, b := range books {
+		if b.Id == id {
+			index = i
+			book = b
+		}
+	}
+	return index, book
+}
