d4481be5e076295832a5003318819256f9a2242f
Erickype <erickype@hotmail.com>
2022-09-28 17:22:50 -0500

    Mutex for read and write and fix of send data

--- data/data.go
+++ data/data.go
@@ -19,33 +19,45 @@ var books = []*Book{
 	{Id: 3, Title: "C", Finished: false},
 	{Id: 4, Title: "D", Finished: false},
 	{Id: 5, Title: "E", Finished: false},
+	{Id: 6, Title: "F", Finished: false},
+	{Id: 7, Title: "G", Finished: false},
+	{Id: 8, Title: "H", Finished: false},
+	{Id: 9, Title: "I", Finished: false},
+	{Id: 10, Title: "J", Finished: false},
 }
 
-func findBook(id int) (int, *Book) {
+func findBook(id int, m *sync.Mutex) (int, *Book) {
 	index := -1
 	var book *Book
 
+	m.Lock()
 	for i, b := range books {
-		if b.Id == id {
+		if b.Id == (id + 1) {
 			index = i
 			book = b
 		}
 	}
+	m.Unlock()
+
 	return index, book
 }
 
-func finishBook(id int) {
-	i, book := findBook(id)
-	if i > 0 {
+func finishBook(id int, m *sync.Mutex) {
+	i, book := findBook(id, m)
+	if i < 0 {
 		return
 	}
+
+	m.Lock()
 	book.Finished = true
 	books[i] = book
-	fmt.Printf("Finished book: %s\n.", book.Title)
+	m.Unlock()
+
+	fmt.Printf("Finished book: %s.\n", book.Title)
 }
 
-func ReadBook(id int, wg *sync.WaitGroup) {
-	finishBook(id)
+func ReadBook(id int, wg *sync.WaitGroup, m *sync.Mutex) {
+	finishBook(id, m)
 
 	delay := rand.Intn(500)
 	time.Sleep(time.Duration(delay) * time.Millisecond)
